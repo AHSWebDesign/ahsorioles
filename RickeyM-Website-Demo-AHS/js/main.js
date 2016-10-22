b46c0a4b487f65a20b339f26baa7fc2fc41607cc
Rickey Markham <Th3DarkAdonis@gmail.com>
2016-10-22 03:03:47 -0500

    Created a small AHSOrioles website demo
    Mainly to show the class a rouch cut of the ahs website.

---
+++ RickeyM-Website-Demo-AHS/js/main.js
@@ -0,0 +1,78 @@
+//Advanced Stuff
+function main() {
+
+(function () {
+   'use strict';
+
+	// Hide .navbar first
+	$(".navbar").hide();
+	
+	// Fade in .navbar
+	$(function () {
+		$(window).scroll(function () {
+            // set distance user needs to scroll before fadeIn on navbar
+			if ($(this).scrollTop() > 250) {
+				$('.navbar').fadeIn();
+			} else {
+				$('.navbar').fadeOut();
+			}
+		});
+
+	
+	});
+	
+	// Preloader */
+	  	$(window).load(function() {
+
+   	// will first fade out the loading animation
+    	$("#status").fadeOut("slow");
+
+    	// will fade out the whole DIV that covers the website.
+    	$("#preloader").delay(500).fadeOut("slow").remove();
+
+  	})
+
+  $(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
+
+    // Show Menu on Book
+    $(window).bind('scroll', function() {
+        var navHeight = $(window).height() - 100;
+        if ($(window).scrollTop() > navHeight) {
+            $('.navbar-default').addClass('on');
+        } else {
+            $('.navbar-default').removeClass('on');
+        }
+    });
+
+    $('body').scrollspy({
+        target: '.navbar-default',
+        offset: 80
+    })
+}());
+
+
+}
+main();
